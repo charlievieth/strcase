8b4c3bb127c9526a57ac095db3d3db8ea68e9ab1
Charlie Vieth <charlie.vieth@gmail.com>
2023-04-08 16:06:52 -0500

    bytealg: fix missing countGeneric on amd64

---
+++ internal/bytealg/count_amd64.go
@@ -0,0 +1,38 @@
+//go:build amd64
+// +build amd64
+
+package bytealg
+
+import (
+	"bytes"
+	"strings"
+)
+
+// A backup implementation to use by assembly.
+func countGeneric(b []byte, c byte) int {
+	if !('A' <= c && c <= 'Z' || 'a' <= c && c <= 'z') {
+		return bytes.Count(b, []byte{c})
+	}
+	n := 0
+	c |= ' '
+	for _, cc := range b {
+		if cc|' ' == c {
+			n++
+		}
+	}
+	return n
+}
+
+func countGenericString(s string, c byte) int {
+	if !('A' <= c && c <= 'Z' || 'a' <= c && c <= 'z') {
+		return strings.Count(s, string(c))
+	}
+	n := 0
+	c |= ' '
+	for i := 0; i < len(s); i++ {
+		if s[i]|' ' == c {
+			n++
+		}
+	}
+	return n
+}
